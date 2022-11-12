e499811403db9d96cc1e54ea7ff23f4371484f55
Nasimi Gunduzlu <pattonsayd@gmail.com>
2022-11-13 01:26:23 +0300

    bloc-event

--- lib/domain/data_providers/user_data_provider.dart
+++ lib/domain/data_providers/user_data_provider.dart
@@ -4,6 +4,7 @@ import '../entity/user.dart';
 
 class UserDataProvider {
   final sharedPreferences = SharedPreferences.getInstance();
+  var imitationCounter = 0;
 
   Future<User> loadValue() async {
     final age = (await sharedPreferences).getInt('age') ?? 0;
@@ -11,6 +12,12 @@ class UserDataProvider {
   }
 
   Future<void> saveValue(User user) async {
+    imitationCounter += 1;
+    if (imitationCounter % 2 == 0) {
+      await Future<dynamic>.delayed(const Duration(seconds: 1));
+    }
+    print(user.age);
+
     (await sharedPreferences).setInt('age', user.age);
   }
 }
